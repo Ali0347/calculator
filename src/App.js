fa84cbf577eb4b6686d69e597aaa32b34e51b331
ALi <princeali4503050@gmail.com>
2023-03-12 21:53:27 +0500

    first comment from ali

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App () {
+
+  const [result , setResult] = useState("");
+  
+  const Controler = (event)=>{
+    setResult(result.concat(event.target.value))
+  }
+  
+  const clearText = ()=>{
+    setResult("")
+  }
+
+  const Calculate = ()=>{
+   setResult(eval(result))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <h1 className='text-center mt-5 text-muted bg-body border-bottom shadow rounded '>Developed By : Ali Bahadur &copy; </h1>
+    <div className="App calCul  "> 
+      <input type="text" placeholder='0' className='rounded' id='shower' value={result} />
+      <input type="button" className='button rounded' value='9' onClick={Controler}/>
+      <input type="button" className='button rounded' value='8' onClick={Controler}/>
+      <input type="button" className='button rounded' value='7' onClick={Controler}/>
+      <input type="button" className='button rounded' value='6'onClick={Controler}/>
+      <input type="button" className='button rounded' value='5'onClick={Controler}/>
+      <input type="button" className='button rounded' value='4'onClick={Controler}/>
+      <input type="button" className='button rounded' value='3'onClick={Controler}/>
+      <input type="button" className='button rounded' value='2'onClick={Controler}/>
+      <input type="button" className='button rounded' value='1' onClick={Controler}/>
+      <input type="button" className='button rounded' value='0' onClick={Controler}/>
+      <input type="button" className='button rounded' value='.' onClick={Controler}/>
+      <input type="button" className='button rounded' value='+' onClick={Controler}/>
+      <input type="button" className='button rounded' value='-' onClick={Controler}/>
+      <input type="button" className='button rounded' value='*' onClick={Controler}/>
+      <input type="button" className='button rounded' value='/' onClick={Controler}/>
+      <input type="button" className='button rounded' value='%' onClick={Controler}/>
+      <input type="button" className='button button2 rounded' value='Clear' onClick={clearText} />
+      <input type="button" className='button button2 rounded' value='=' onClick={Calculate}/>
+    </div>
     </div>
   );
 }
